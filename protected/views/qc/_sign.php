56ad1c25dc77a7cc39986c5092e20494d4b32cf8
LBS IT <percylee@lbsgroup.com.hk>
2021-05-12 10:29:09 +0800

    品檢調整簽字

--- protected/views/qc/_sign.php
+++ protected/views/qc/_sign.php
@@ -1,11 +1,12 @@
 <?php
 	$button = TbHtml::button(Yii::t('qc','Clear'), array('name'=>'btnClearSignature','id'=>'btnClearSignature','class'=>'btn-sm',));
+	$button .= TbHtml::button(Yii::t('qc','Adjust'), array('name'=>'btnAlign','id'=>'btnAlign','class'=>'btn-sm',));
 	$hidden = TbHtml::hiddenField('sign_target_field','');
 	$content = <<<EOF
 <div class='row'>
 	<div class='col-sm-7'>
 		$hidden
-		<canvas id='qc-signature' class='signature-pad' style='border:1px solid black; width:300px; height:150px'></canvas>
+		<canvas id='qc-signature' class='signature-pad' style='border:1px solid black; width:330px; height:200px'></canvas>
 		$button
 	</div>
 </div>
@@ -38,8 +39,20 @@ EOF;
 <?php
 $js = <<<EOF
 var signaturePad = new SignaturePad(document.getElementById('qc-signature'));
+function resizeCanvas() {
+	var canvas = document.getElementById('qc-signature');
+    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio, ratio);
+    signaturePad.clear(); // otherwise isEmpty() might return incorrect value
+}
+
 $('#btnClearSignature').on('click',function(){
 	signaturePad.clear();
+});
+$('#btnAlign').on('click',function(){
+	resizeCanvas();
 });
 $('#btnOkSignature').on('click',function(){
 	var inputid = $('#sign_target_field').val();
@@ -49,6 +62,10 @@ $('#btnOkSignature').on('click',function(){
 	$('#'+inputid+'_img').attr('src',data);
 	signaturePad.clear();
 });
+
+//window.addEventListener("resize", resizeCanvas);
+//resizeCanvas();
+//window.onresize = resizeCanvas;
 EOF;
 Yii::app()->clientScript->registerScript('signatureDialog',$js,CClientScript::POS_READY);
 ?>
